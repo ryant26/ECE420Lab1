79ac106d8af333ed647f5232cc4f4819128e5c99
ryant26 <ryant26@gmail.com>
2016-01-13 20:51:56 -0700

    parsing threads finished

--- src/multiply.c
+++ src/multiply.c
@@ -12,10 +12,30 @@ int parse_number_threads(int argc, char** argv){
 	if (argc != 2){
 		printf("You didn't enter the correct number of arguments\n");
 		print_usage();
+
+		exit(EXIT_FAILURE);
 	}
+
+	int threads = atoi(argv[1]);
+	if (threads < 1 || !check_square(threads)){
+		printf("You entered an invalid number of threads\n");
+		printf("Please select a square integer greater than 1\n");
+		print_usage();		
+	}
+
 	return 0;
 }
 
 void print_usage(){
-	printf("USAGE: mulitply <number of threads> \n");
+	printf("USAGE: mulitply <square number of threads> \n");
+}
+
+int check_square(int number){
+	int i;
+	for(i = 0; i <= number; i++){
+		if (number == i * i){
+			return 1;
+		}
+	}
+	return 0;
 }
--- src/multiply.h
+++ src/multiply.h
@@ -1,8 +1,20 @@
 #ifndef MULTIPLY_H
 #define MULTIPLY_H
 
+// The function passed to pthread_create
+// Params: threadID
 void* multiplyBlock(void*);
+
+// The function to parse the number of threads from the 
+// command line
+// Params: int argc, char** argv
 int parse_number_threads(int, char**);
+
+//Prints the usage of the binary from the terminal
 void print_usage();
+
+// Tests if the input ineger is a square number
+// Params: int number to check
+int check_square(int);
 
 #endif
